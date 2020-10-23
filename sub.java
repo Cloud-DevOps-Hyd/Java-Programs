78b03c87a7ea365e959b273ec6adf107da6f23d8
ravindra <ravindra.devops@gmail.com>
2020-10-23 07:49:29 +0530

    sub.java V1.0.2

--- sub.java
+++ sub.java
@@ -1,6 +1,26 @@
-class sub
+import java.util.Scanner;
+public class sub
 {
 public static void main(String [] args)
 {
-int a,b,c;
+int num1, num2, sum;
+
+Scanner sc = new Scanner(System.in);
+
+System.out.println("Enter the First Number:");
+num1 = sc.nextInt();
+
+System.out.println("ENter the Second Number:");
+num2 = sc.nextInt();
+
+sc.close();
+
+sum = num1 + num2;
+System.out.println("The sum of Two values are "+sum);
+
+}
+
+}
+
+
 
